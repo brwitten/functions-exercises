58440268c506e6a6a79a5662833b383bcc6060d6
brwitten <bethrwitten@gmail.com>
2017-09-28 12:28:34 -0700

    Finished #2

--- functions.js
+++ functions.js
@@ -2,12 +2,22 @@
 
 // 1. Build your own concatenation
 
-function combineWords(x,y) {
+function combineWords(word1,word2) {
   console.log(x+y);
 }
 
-var x = "jumping"
-var y = "elephants"
+var word1 = "jumping"
+var word2 = "elephants"
 combineWords(x,y)
 
-// 
+// 2. Repeat a phrase
+
+function parrot(phrase, n) {
+  while (n > 0) {
+      console.log(phrase);
+      n = n-1;
+      parrot();
+    }
+}
+
+parrot("hello!",3)
