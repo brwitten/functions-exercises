71acaf3bd9d4d9dbc846ac56b4702ff28d6fcdd3
brwitten <bethrwitten@gmail.com>
2017-09-28 12:06:37 -0700

    finished #1

--- functions.js
+++ functions.js
@@ -1,1 +1,13 @@
 // TODO: add your solutions here!
+
+// 1. Build your own concatenation
+
+function combineWords(x,y) {
+  console.log(x+y);
+}
+
+var x = "jumping"
+var y = "elephants"
+combineWords(x,y)
+
+// 
